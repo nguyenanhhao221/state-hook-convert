66796bd6e4b86ae8ca2c262193625777aeb2daf6
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-03-10 00:14:12 +0700

    remove App Class from index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import AppClass from "./Components/AppClass";
 import './index.css';
 import AppFunction from "./Components/AppFunction";
 
 ReactDOM.render(
-  // <AppClass />,
   <AppFunction />,
   document.getElementById("app")
 );
